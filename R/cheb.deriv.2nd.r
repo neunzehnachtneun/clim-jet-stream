19e2a8ebc98f5cfd546ea996557a6435cb2a5a8b
sebastian kiefer <s.kiefer@posteo.de>
2016-05-19 10:25:14 +0200

    ...

--- R/cheb.deriv.2nd.r
+++
@@ -1,8 +0,0 @@
-####################################################################################################
-## source('~/Master_Thesis/pckg.cheb/R/cheb.deriv.2nd.r')
-##
-##
-
-##
-## Werte der zweiten Ableitung des Modells
-cheb.deriv.2nd <- function() {}
--- R/cheb.fit.float.r
+++
@@ -1,9 +0,0 @@
-####################################################################################################
-## source('~/Master_Thesis/pckg.cheb/R/cheb.fit.float.r')
-##
-##
-
-## Variante 3 - Least squares fit über 'fließende' sequenzierungen
-##
-
-cheb.fit.float <- function() {}
