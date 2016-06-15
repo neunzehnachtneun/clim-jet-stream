27162df3b28dd1bc43661f5a038e48cd25f414e1
sebastian kiefer <s.kiefer@posteo.de>
2016-06-15 10:55:03 +0200

    Funktion für Fit über Sequenzen wieder implementiert. - noch unvollständig!

--- pckg.cheb/R/functions-chebyshev.r
+++ pckg.cheb/R/functions-chebyshev.r
@@ -1,10 +1,11 @@
 ## source('~/Master_Thesis/pckg.cheb/R/pckg.cheb.r')
 ##
+## library(devtools)
+## library(roxygen2)
 ##
-
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+## Build and Reload Package:  'Ctrl + Shift + B'
+## Check Package:             'Ctrl + Shift + E'
+## Test Package:              'Ctrl + Shift + T'
 
 
 ##
@@ -183,8 +184,8 @@ cheb.deriv.2nd <- function(x.axis, cheb.coeff) {
 cheb.fit <- function(d, x.axis, n){
   x.cheb <- cheb.scale(x.axis)
   cheb.t <- cheb.1st(x.axis, n)
-  cheb.u <- cheb.2nd(x.axis, n)
-  m <- n + 1
+#  cheb.u <- cheb.2nd(x.axis, n)
+#  m <- n + 1
   ## modell berechnungen
   # berechnung der koeffizienten des polyfits
   cheb.coeff <- solve(t(cheb.t) %*% cheb.t) %*% t(cheb.t) %*% d
@@ -205,83 +206,69 @@ cheb.fit <- function(d, x.axis, n){
 
 
 
-# ##
-# #' @title Curve Fitting with Chebyshev Polynomials over Sequences
-# #' @description
-# #' Fittet ein Chebyshev Polynom beliebiger Ordnung an einen sequenzierten Datensatz/Zeitreihe mittels Least Squares Verfahren
-# #' @examples
-# #' cheb.fit.seq(d, x.axis, n, split)
-# cheb.fit.seq <- function(d, x.axis, n, split, dx.hr){
-#   ## funktion zur rechnung mit chebyshev polynomen
-#   ## bestimmung der polynome erster und zweiter art, polynomfit nter ordnung,
-#   ## ermittelung der ableitung der polynome und entwicklung von modellen des fits und der ableitung.
-#   ## inputs: d datensatz, der approximiert werden soll,
-#   ## x stützpunkte für f(x), zb längengrad [0,90],
-#   ## n ordnung des polynom fit, dx.h auflösung des hochaufgelösten gitters,
-#   ## split die unterteilung der daten in sektoren mit der länge split
-#   ## outputs: koeffizienten und ableitung, modell und ableitung
-#   ##
-#
-#   x.mat <- t(matrix(x,ncol = split))
-#   d.mat <- t(matrix(d,ncol = split))
-#
-#   # schleife über sequenzen des Datensatzes
-#   for (i in 1:length(x.mat[,1])) {
-#     # print(i)
-#     # erstellung der sequenzen
-#     x.seq <- if (i == 1) c(x.mat[i,]) else c(x.mat[(i - 1), dim(x.mat)[2]], x.mat[i,])
-#     d.seq <- if (i == 1) c(d.mat[i,]) else c(d.mat[(i - 1), dim(d.mat)[2]], d.mat[i,])
-#     # skalierung der stützpunkte auf [-1,1]
-#     x.cheb <- fkt.cheb.scale(x.seq)
-#     # polynom *nullter* ordnung äquivalent zu *erstem* eintrag in vektor
-#     # daher wird ein hilfsskalar benutzt
-#     m <- (n + 1)
-#     ## erzeugung der chebyshev polynome erster art
-#     cheb.t <- fkt.cheb.1st(x.cheb, n)
-#
-#     ## erzeugung der chebyshev polynome zweiter art
-#     cheb.u <- fkt.cheb.2nd(x.cheb, n)
-#
-#     # berechnung der ableitung der polynome erster art
-#     # rekursionsformel wiki???
-#     # dT/dx = n * U_(n-1)
-#     cheb.t.deriv <- t((2:m)*t(cheb.u[,2:m]))
-#
-#     ## modell berechnungen
-#     # berechnung der koeffizienten des polyfits
-#     cheb.coeff.seq <- solve(t(cheb.t) %*% cheb.t) %*% t(cheb.t) %*% d.seq
-#     cheb.coeff <- if (i == 1) cheb.coeff.seq else cbind(cheb.coeff, cheb.coeff.seq)
-#     # berechnung des gefilterten modells
-#     cheb.model.seq <- cheb.t %*% cheb.coeff.seq
-#     end <- length(cheb.model.seq)
-#     cheb.model <- if (i == 1) cheb.model.seq else c(cheb.model, cheb.model.seq[2:end])
-#     # berechnung des abgeleiteten modells
-#     cheb.model.deriv.seq <- cheb.t.deriv %*% cheb.coeff.seq[2:m]
-#     cheb.model.deriv <- if (i == 1) cheb.model.deriv.seq else c(cheb.model.deriv, cheb.model.deriv.seq[2:end])
-#
-#     # berechnung der nullstellen
-#     extr.seq <- uniroot.all(fkt.cheb.deriv, cheb.coeff = cheb.coeff.seq, lower = (-1), upper = 1)
-#     # reskalierung der Nullstellen auf normale Lat- Achse
-#     x.extr.seq <- fkt.cheb.rescale(extr.seq, x = x.seq)
-#     x.extr <- if (i == 1) x.extr.seq else c(x.extr, x.extr.seq)
-#     # filtern der maxima aus nullstellen d. ableitung
-#     y.extr.seq <- if (length(extr.seq) != 0) fkt.cheb.val(x = extr.seq, cheb.coeff = cheb.coeff.seq)
-#     #y.extr <- if (exists(y.extr) == F &) y.extr.seq else c(y.extr, y.extr.seq)
-#     if (exists("y.extr.seq") == T & exists("y.extr") == F) {
-#       y.extr <- y.extr.seq
-#     } else if (exists("y.extr.seq") == T & exists("y.extr") == T) {
-#       y.extr <- c(y.extr, y.extr.seq)
-#     }
-#     ## löschung der übergangsvariablen
-#     #    print(i)
-#     #    print(cheb.coeff.seq)
-#     rm(cheb.coeff.seq, cheb.model.seq, cheb.model.deriv.seq, extr.seq, x.extr.seq, y.extr.seq)
-#
-#     #print(i)
-#   }
-#
-#   ## übergabe der variablen als liste
-#   cheb.list <- list(cheb.coeff = cheb.coeff, cheb.model = cheb.model, cheb.model.deriv = cheb.model.deriv, extr.x = x.extr, extr.y = y.extr)
-#   return(cheb.list)
-# }
-#
+##
+#' @title Curve Fitting with Chebyshev Polynomials over Sequences
+#' @description
+#' Fittet ein Chebyshev Polynom beliebiger Ordnung an einen sequenzierten Datensatz/Zeitreihe mittels Least Squares Verfahren
+#' @examples
+#' cheb.fit.seq(d, x.axis, n, l)
+cheb.fit.seq <- function(d, x.axis, n, l){
+  ## funktion zur rechnung mit chebyshev polynomen
+  ## bestimmung der polynome erster und zweiter art, polynomfit nter ordnung,
+  ## ermittelung der ableitung der polynome und entwicklung von modellen des fits und der ableitung.
+  ## inputs: d datensatz, der approximiert werden soll,
+  ## x stützpunkte für f(x), zb längengrad [0,90],
+  ## n ordnung des polynom fit, dx.h auflösung des hochaufgelösten gitters,
+  ## split die unterteilung der daten in sektoren mit der länge split
+  ## outputs: koeffizienten und ableitung, modell und ableitung
+  ##
+
+  x.mat <- matrix(x.axis, ncol = l, byrow = TRUE)
+  d.mat <- matrix(d, ncol = l, byrow = TRUE)
+
+  # schleife über sequenzen des Datensatzes
+  for (i in 1:length(x.mat[,1])) {
+    # print(i)
+    # erstellung der sequenzen
+    x.seq <- if (i == 1) c(x.mat[i,]) else c(x.mat[(i - 1), dim(x.mat)[2]], x.mat[i,])
+    x.cheb.seq <- cheb.scale(x.seq)
+    d.seq <- if (i == 1) c(d.mat[i,]) else c(d.mat[(i - 1), dim(d.mat)[2]], d.mat[i,])
+    # skalierung der stützpunkte auf [-1,1]
+    cheb.t.seq <- cheb.1st(x.seq, n)
+    ######################################################################
+    ######################################################################
+    ## modell berechnungen
+    # berechnung der koeffizienten des polyfits
+    cheb.coeff.seq <- solve(t(cheb.t.seq) %*% cheb.t.seq) %*% t(cheb.t.seq) %*% d.seq
+    cheb.coeff <- if (i == 1) cheb.coeff.seq else cbind(cheb.coeff, cheb.coeff.seq)
+    # berechnung des gefilterten modells
+    cheb.model.seq <- cheb.model(x.cheb.seq, cheb.coeff.seq)
+    cheb.model <- if (i == 1) cheb.model.seq else c(cheb.model, cheb.model.seq[2:l+1])
+    # berechnung des abgeleiteten modells
+    cheb.model.deriv.1st.seq <- cheb.deriv.1st(x.cheb.seq, cheb.coeff.seq)
+    cheb.model.deriv.1st <- if (i == 1) cheb.model.deriv.1st.seq else c(cheb.model.deriv.1st, cheb.model.deriv.1st.seq[2:l+1])
+
+    # berechnung der nullstellen
+    extr.seq <- rootSolve::uniroot.all(cheb.deriv.1st, cheb.coeff = cheb.coeff.seq, lower = (-1), upper = 1)
+    # reskalierung der Nullstellen auf normale Lat- Achse
+    x.extr.seq <- cheb.rescale(extr.seq, x.axis = x.seq)
+    y.extr.seq <- if (length(extr.seq) != 0) cheb.model(x.axis = extr.seq, cheb.coeff = cheb.coeff)
+
+    if (exists("x.extr.seq") == TRUE & exists("x.extr") == FALSE) {
+      x.extr <- x.extr.seq
+      y.extr <- y.extr.seq
+    } else if (exists("x.extr.seq") == TRUE & exists("x.extr") == TRUE) {
+      x.extr <- c(x.extr, x.extr.seq)
+      y.extr <- c(y.extr, y.extr.seq)
+    }
+
+
+
+    ######################################################################
+  }
+
+  ## übergabe der variablen als liste
+  cheb.list <- list(cheb.model = cheb.model, cheb.model.deriv.1st = cheb.model.deriv.1st, extr.x = x.extr, extr.y = y.extr)
+  return(cheb.list)
+}
+
