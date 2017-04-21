268fb7d7bc4b0930cb6e11e9a557c22b06f7f6c5
Sebastian Kiefer <s.kiefer@posteo.de>
2017-04-21 14:27:40 +0200

    Apply und Loop für alle Zeitschritte eingefügt. Test auf Server erforderlich.

--- b-analyse_monthly_change.r
+++ b-analyse_monthly_change.r
@@ -26,22 +26,52 @@ dts = as.POSIXct(date.help*3600, origin = '1900-01-01 00:00', tz = 'UTC')
 dts.month <- months(dts, abbreviate = TRUE)
 dts.year <- years(dts)
 
+# Unterscheidung von warmen & kalten Monaten
+dts.cld.wrm <- rep(NA, length.out = length(dts))
+dts.cld.wrm[which(dts.month == "Nov" | dts.month == "Dec" | dts.month == "Jan" | dts.month == "Feb" | dts.month == "Mar" | dts.month == "Apr")] <- "cold"
+dts.cld.wrm[which(dts.month == "May" | dts.month == "Jun" | dts.month == "Jul" | dts.month == "Aug" | dts.month == "Sep" | dts.month == "Oct")] <- "warm"
+
+# Unterscheidung von Jahreszeiten
+dts.season <- rep(NA, length.out = length(dts))
+dts.season[which(dts.month == "Dec" | dts.month == "Jan" | dts.month == "Feb")] <- "DJF"
+dts.season[which(dts.month == "Mar" | dts.month == "Apr" | dts.month == "May")] <- "MAM"
+dts.season[which(dts.month == "Jun" | dts.month == "Jul" | dts.month == "Aug")] <- "JJA"
+dts.season[which(dts.month == "Sep" | dts.month == "Oct" | dts.month == "Nov")] <- "SON"
+
 ## VERARBEITEN DER DATEN ####
 ## 
 source("e-jetstream_detection_schemes.r")
 source("n-help-functions.r")
+# cluster für paralleles Prozessieren
+cl <- makeCluster(getOption("cl.cores", 2))
 
-## METHODE 1: find.jet.maximum ####
+####
+## SCHLEIFE ÜBER DRUCK LEVEL ####
+####
+for (p.lvl in 3:6) {
+  print(lev[p.lvl])
+}; p.lvl <- 3
+## ZEITSCHRITTE
+t.stp <- 1; print(paste(dts.month[t.stp], dts.year[t.stp]))
+#
+
+## METHODE 0: find.jet.maximum ####
 ## 
-find.jet.maximum(mat = u[,,1,1], axis = lat)
+#m0 <- find.jet.maximum(matrix = u[,,p.lvl, t.stp], axis = lat)
 
-## METHODE 2: find.jet.chebpoly
+## METHODE 1: find.jet.chebpoly
 ##
-find.jet.chebpoly(array = u[,,1,1], axis = lat)
-
-## METHODE 3: find.jet.dijkstra.2d
+m1.t <- find.jets.chebpoly(matrix = u[,, p.lvl, t.stp], axis = lat, n.order = 8)
+# plot(m2$MaxJ.lat - m1$MaxJ.lat)
+# print(mean(m2$MaxJ.lat - m1$MaxJ.lat, na.rm = TRUE))
+m1 <- clusterApply(cl, u[,,p.lvl,], fun = find.jets.chebpoly, c(1,3), axis = lat)
+stopCluster(cl)
+## METHODE 2: find.jet.dijkstra.2d
 ## 
-dts.month[1]
-find.jet.dijkstra.2d(u = u[,,1,1], v = v[,,1,1], lon = lon, lat = lat, jet = "STJ", season = "warm")
+m2 <- NULL
+for (i in length(dts)) {
+  m2[i] <- find.jets.dijkstra.2d(u = u[,,1,1], v = v[,,1,1], lon = lon, lat = lat, season = dts.cld.wrm[t.stp])
+}
+
 
 
