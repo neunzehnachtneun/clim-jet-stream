324ed93fe833bf78bfe388d2b722762475fabeac
Sebastian Kiefer <s.kiefer@posteo.de>
2017-09-08 18:54:28 +0200

    Paketumstieg auf lubridate für Posix etc., Entfernen der Subdataframes (werden nun vor den Plotroutinen erstellt), Einstieg in tibbles statt dataframes.

--- ab-monthly_format_data.r
+++ ab-monthly_format_data.r
@@ -19,14 +19,24 @@ ls()
 # Nachladen der Packages
 # library(ncdf4); library(chron); library(parallel); library(foreach); library(doParallel);
 
-## Laden des Sea Ice Index Datensatzes ####
+## LADEN DES SEA ICE DATENSATZTES ####
 ##
-sea.ice.index <- read.csv("04-data-nc/d-sea-ice-index-arctic-noaa.csv")
+library(lubridate)
 
+df.sea.ice <- read.csv("04-data-nc-csv/d-sea-ice-index-arctic-noaa.csv")
+
+df.sea.ice$dts <- as.Date(paste0(df.sea.ice$year,"-",df.sea.ice$mo,"-1"))
+df.sea.ice$year <- year(df.sea.ice$dts)
+df.sea.ice$month <- as.character(month(df.sea.ice$dts, label = TRUE))
+
+names(df.sea.ice)
+df.sea.ice <- df.sea.ice[c(-1, -3, -4, -5, -8)]
+names(df.sea.ice)
 
 ## FORMATIEREN DER DATENSATZE ####
 ## 
-library(plyr); library(dplyr); library(reshape2)
+# library(plyr); 
+library(dplyr); library(reshape2)
 
 ## Globales Maximum M1
 # Maximaljet
@@ -35,7 +45,6 @@ m1.J.lat    <- sapply(m1, "[[", "MaxJ.lat"); colnames(m1.J.lat)  <- dts; rowname
 m2b.J.lat <- sapply(m2, "[[", "MaxJ.lat");   colnames(m2b.J.lat) <- dts; rownames(m2b.J.lat) <- lon;
 
 ## Chebyshev
-
 ## Polarfrontjet
 m2c.PFJ.lat <- sapply(m2, "[[", "PFJ.lat"); colnames(m2c.PFJ.lat) <- dts; rownames(m2c.PFJ.lat) <- lon;
 m2c.PFJ.u   <- sapply(m2, "[[", "PFJ.u");   colnames(m2c.PFJ.u)   <- dts; rownames(m2c.PFJ.u)   <- lon;
@@ -68,212 +77,193 @@ m3.SJS.v    <- sapply(m3, "[[", "SJS.v");   colnames(m3.SJS.v)   <- dts; rowname
 
 ## MELTEN DER DATENSÄTZE (RESHAPE2::MELT) FÜR GGPLOT2 ####
 ## Maximaljet max()
-df.jets.month.m1.mj <- melt(m1.J.lat, varnames = c("lon", "dts"),value.name = "lat" )
+df.jets.month.m1.mj <- melt(m1.J.lat, varnames = c("lon", "dts"),  value.name = "lat" )
 df.jets.month.m1.mj$dts          <- rep(dts, each = n.lon)          # Datum
 df.jets.month.m1.mj$year         <- rep(dts.year, each = n.lon)     # Jahr
 df.jets.month.m1.mj$month        <- rep(dts.month, each = n.lon)    # Monat
 df.jets.month.m1.mj$season       <- rep(dts.season, each = n.lon)   # Jahreszeit/Saison
+## Methode und Klassifizierung
+df.jets.month.m1.mj$method <- "Max"
+df.jets.month.m1.mj$class <- "MJ"
 # Umsortieren der Spalten des Datensatzes
-df.jets.month.m1.mj <- df.jets.month.m1.mj[,c("dts", "year", "month", "season", "lon", "lat")]
+df.jets.month.m1.mj <- df.jets.month.m1.mj[,c("dts", "year", "month", "season", "method", "class", "lon", "lat")]
 # Einfügen der u, v, z -Reihen
 df.jets.month.m1.mj$u  <- NA
 df.jets.month.m1.mj$v  <- NA
 df.jets.month.m1.mj$uv <- NA
 df.jets.month.m1.mj$z  <- NA
-df.jets.month.m1.mj$type <- "MJ-max"
+
+
 ## Maximaljet Chebyshev
 df.jets.month.m2.mj <- melt(m2b.J.lat, varnames = c("lon", "dts"),value.name = "lat" )
 df.jets.month.m2.mj$dts          <- rep(dts, each = n.lon)          # Datum
 df.jets.month.m2.mj$year         <- rep(dts.year, each = n.lon)     # Jahr
 df.jets.month.m2.mj$month        <- rep(dts.month, each = n.lon)    # Monat
 df.jets.month.m2.mj$season       <- rep(dts.season, each = n.lon)   # Jahreszeit/Saison
+## Methode und Klassifizierung
+df.jets.month.m2.mj$method <- "Chebyshev"
+df.jets.month.m2.mj$class <- "MJ"
 # Umsortieren der Spalten des Datensatzes
-df.jets.month.m2.mj <- df.jets.month.m2.mj[,c("dts", "year", "month", "season", "lon", "lat")]
+df.jets.month.m2.mj <- df.jets.month.m2.mj[,c("dts", "year", "month", "season", "method", "class", "lon", "lat")]
 # Einfügen der u, v, z -Reihen
 df.jets.month.m2.mj$u  <- NA
 df.jets.month.m2.mj$v  <- NA
 df.jets.month.m2.mj$uv <- NA
 df.jets.month.m2.mj$z  <- NA
-df.jets.month.m2.mj$type <- "MJ-Chebyshev"
+
 ## PFJ || Chebyshev- Methode 
 df.jets.month.m2.pfj <- melt(m2c.PFJ.lat, varnames = c("lon", "dts"),value.name = "lat" )
 df.jets.month.m2.pfj$dts          <- rep(dts, each = n.lon)          # Datum
 df.jets.month.m2.pfj$year         <- rep(dts.year, each = n.lon)     # Jahr
 df.jets.month.m2.pfj$month        <- rep(dts.month, each = n.lon)    # Monat
 df.jets.month.m2.pfj$season       <- rep(dts.season, each = n.lon)   # Jahreszeit/Saison
+## Methode und Klassifizierung
+df.jets.month.m2.pfj$method <- "Chebyshev"
+df.jets.month.m2.pfj$class <- "PFJ"
 # Umsortieren der Spalten des Datensatzes
-df.jets.month.m2.pfj <- df.jets.month.m2.pfj[,c("dts", "year", "month", "season", "lon", "lat")]
+df.jets.month.m2.pfj <- df.jets.month.m2.pfj[,c("dts", "year", "month", "season", "method", "class", "lon", "lat")]
 # Einfügen der u, v, z -Reihen
 df.jets.month.m2.pfj$u  <- melt(m2c.PFJ.u)$value
 df.jets.month.m2.pfj$v  <- melt(m2c.PFJ.v)$value
 df.jets.month.m2.pfj$uv <- sqrt(df.jets.month.m2.pfj$u**2 + df.jets.month.m2.pfj$v**2)
 df.jets.month.m2.pfj$z  <- melt(m2c.PFJ.z)$value
-df.jets.month.m2.pfj$type <- "PFJ-Chebyshev"
+
+
 ## STJ || Chebyshev- Methode 
 df.jets.month.m2.stj <- melt(m2c.STJ.lat, varnames = c("lon", "dts"),value.name = "lat" )
 df.jets.month.m2.stj$dts          <- rep(dts, each = n.lon)          # Datum
 df.jets.month.m2.stj$year         <- rep(dts.year, each = n.lon)     # Jahr
 df.jets.month.m2.stj$month        <- rep(dts.month, each = n.lon)    # Monat
 df.jets.month.m2.stj$season       <- rep(dts.season, each = n.lon)   # Jahreszeit/Saison
+## Methode und Klassifizierung
+df.jets.month.m2.stj$method <- "Chebyshev"
+df.jets.month.m2.stj$class <- "STJ"
 # Umsortieren der Spalten des Datensatzes
-df.jets.month.m2.stj <- df.jets.month.m2.stj[,c("dts", "year", "month", "season", "lon", "lat")]
+df.jets.month.m2.stj <- df.jets.month.m2.stj[,c("dts", "year", "month", "season", "method", "class", "lon", "lat")]
 # Einfügen der u, v, z -Reihen
 df.jets.month.m2.stj$u  <- melt(m2c.STJ.u)$value
 df.jets.month.m2.stj$v  <- melt(m2c.STJ.v)$value
 df.jets.month.m2.stj$uv <- sqrt(df.jets.month.m2.stj$u**2 + df.jets.month.m2.stj$v**2)
 df.jets.month.m2.stj$z  <- melt(m2c.STJ.z)$value
-df.jets.month.m2.stj$type <- "STJ-Chebyshev"
+
+
 ## SJS || Chebyshev- Methode 
 df.jets.month.m2.sjs <- melt(m2c.SJS.lat, varnames = c("lon", "dts"),value.name = "lat" )
 df.jets.month.m2.sjs$dts          <- rep(dts, each = n.lon)          # Datum
 df.jets.month.m2.sjs$year         <- rep(dts.year, each = n.lon)     # Jahr
 df.jets.month.m2.sjs$month        <- rep(dts.month, each = n.lon)    # Monat
 df.jets.month.m2.sjs$season       <- rep(dts.season, each = n.lon)   # Jahreszeit/Saison
+## Methode und Klassifizierung
+df.jets.month.m2.sjs$method <- "Chebyshev"
+df.jets.month.m2.sjs$class <- "SJ"
 # Umsortieren der Spalten des Datensatzes
-df.jets.month.m2.sjs <- df.jets.month.m2.sjs[,c("dts", "year", "month", "season", "lon", "lat")]
+df.jets.month.m2.sjs <- df.jets.month.m2.sjs[,c("dts", "year", "month", "season", "method", "class", "lon", "lat")]
 # Einfügen der u, v, z -Reihen
 df.jets.month.m2.sjs$u  <- melt(m2c.SJS.u)$value
 df.jets.month.m2.sjs$v  <- melt(m2c.SJS.v)$value
 df.jets.month.m2.sjs$uv <- sqrt(df.jets.month.m2.sjs$u**2 + df.jets.month.m2.sjs$v**2)
 df.jets.month.m2.sjs$z  <- melt(m2c.SJS.z)$value
-df.jets.month.m2.sjs$type <- "SJS-Chebyshev"
+
+
 ## PFJ || Dijkstra- Methode 
 df.jets.month.m3.pfj <- melt(m3.PFJ.lat, varnames = c("lon", "dts"),value.name = "lat" )
 df.jets.month.m3.pfj$dts          <- rep(dts, each = n.lon)          # Datum
 df.jets.month.m3.pfj$year         <- rep(dts.year, each = n.lon)     # Jahr
 df.jets.month.m3.pfj$month        <- rep(dts.month, each = n.lon)    # Monat
 df.jets.month.m3.pfj$season       <- rep(dts.season, each = n.lon)   # Jahreszeit/Saison
+## Methode und Klassifizierung
+df.jets.month.m3.pfj$method <- "Dijkstra"
+df.jets.month.m3.pfj$class <- "PFJ"
 # Umsortieren der Spalten des Datensatzes
-df.jets.month.m3.pfj <- df.jets.month.m3.pfj[,c("dts", "year", "month", "season", "lon", "lat")]
+df.jets.month.m3.pfj <- df.jets.month.m3.pfj[,c("dts", "year", "month", "season", "method", "class", "lon", "lat")]
 # Einfügen der u, v, z -Reihen
 df.jets.month.m3.pfj$u  <- melt(m3.PFJ.u)$value
 df.jets.month.m3.pfj$v  <- melt(m3.PFJ.v)$value
 df.jets.month.m3.pfj$uv <- sqrt(df.jets.month.m3.pfj$u**2 + df.jets.month.m3.pfj$v**2)
 df.jets.month.m3.pfj$z  <- NA
-df.jets.month.m3.pfj$type <- "PFJ-Dijkstra"
+
+
 ## STJ || Dijkstra- Methode 
 df.jets.month.m3.stj <- melt(m3.STJ.lat, varnames = c("lon", "dts"),value.name = "lat" )
 df.jets.month.m3.stj$dts          <- rep(dts, each = n.lon)          # Datum
 df.jets.month.m3.stj$year         <- rep(dts.year, each = n.lon)     # Jahr
 df.jets.month.m3.stj$month        <- rep(dts.month, each = n.lon)    # Monat
 df.jets.month.m3.stj$season       <- rep(dts.season, each = n.lon)   # Jahreszeit/Saison
+## Methode und Klassifizierung
+df.jets.month.m3.stj$method <- "Dijkstra"
+df.jets.month.m3.stj$class <- "STJ"
 # Umsortieren der Spalten des Datensatzes
-df.jets.month.m3.stj <- df.jets.month.m3.stj[,c("dts", "year", "month", "season", "lon", "lat")]
+df.jets.month.m3.stj <- df.jets.month.m3.stj[,c("dts", "year", "month", "season", "method", "class", "lon", "lat")]
 # Einfügen der u, v, z -Reihen
 df.jets.month.m3.stj$u  <- melt(m3.STJ.u)$value
 df.jets.month.m3.stj$v  <- melt(m3.STJ.v)$value
 df.jets.month.m3.stj$uv <- sqrt(df.jets.month.m3.stj$u**2 + df.jets.month.m3.stj$v**2)
 df.jets.month.m3.stj$z  <- NA
-df.jets.month.m3.stj$type <- "STJ-Dijkstra"
+
+
 ## SJS || Dijkstra- Methode 
 df.jets.month.m3.sjs <- melt(m3.SJS.lat, varnames = c("lon", "dts"),value.name = "lat" )
 df.jets.month.m3.sjs$dts          <- rep(dts, each = n.lon)          # Datum
 df.jets.month.m3.sjs$year         <- rep(dts.year, each = n.lon)     # Jahr
 df.jets.month.m3.sjs$month        <- rep(dts.month, each = n.lon)    # Monat
 df.jets.month.m3.sjs$season       <- rep(dts.season, each = n.lon)   # Jahreszeit/Saison
+## Methode und Klassifizierung
+df.jets.month.m3.sjs$method <- "Dijkstra"
+df.jets.month.m3.sjs$class <- "SJ"
 # Umsortieren der Spalten des Datensatzes
-df.jets.month.m3.sjs <- df.jets.month.m3.sjs[,c("dts", "year", "month", "season", "lon", "lat")]
+df.jets.month.m3.sjs <- df.jets.month.m3.sjs[,c("dts", "year", "month", "season", "method", "class", "lon", "lat")]
 # Einfügen der u, v, z -Reihen
 df.jets.month.m3.sjs$u  <- melt(m3.SJS.u)$value
 df.jets.month.m3.sjs$v  <- melt(m3.SJS.v)$value
 df.jets.month.m3.sjs$uv <- sqrt(df.jets.month.m3.sjs$u**2 + df.jets.month.m3.sjs$v**2)
 df.jets.month.m3.sjs$z  <- NA
-df.jets.month.m3.sjs$type <- "SJS-Dijkstra"
+
+
 
 ## MERGEN DER DATAFRAMES ####
 ## 
-df.jets.month <- merge(df.jets.month.m1.mj, df.jets.month.m2.mj, all = TRUE)
-df.jets.month <- merge(df.jets.month, df.jets.month.m2.pfj, all = TRUE)
-df.jets.month <- merge(df.jets.month, df.jets.month.m2.stj, all = TRUE)
-df.jets.month <- merge(df.jets.month, df.jets.month.m2.sjs, all = TRUE)
-df.jets.month <- merge(df.jets.month, df.jets.month.m3.pfj, all = TRUE)
-df.jets.month <- merge(df.jets.month, df.jets.month.m3.stj, all = TRUE)
-df.jets.month <- merge(df.jets.month, df.jets.month.m3.sjs, all = TRUE)
+## Mergen der einzelnen Dataframes der unterschiedlichen Methoden
+df.jets.month <- rbind(df.jets.month.m1.mj, df.jets.month.m2.mj,
+                       df.jets.month.m2.pfj, df.jets.month.m2.stj, df.jets.month.m2.sjs,
+                       df.jets.month.m3.pfj, df.jets.month.m3.stj, df.jets.month.m3.sjs)
+# Sortieren nach Datum und Längengrad
+df.jets.month <- df.jets.month[order(df.jets.month$dts, df.jets.month$lon),]
+# Umformatieren der year-Datenreihe als numeric 
+df.jets.month$year <- as.numeric(as.character(df.jets.month$year))
 
-rm(df.jets.month.m1.mj, df.jets.month.m2.mj, 
-   df.jets.month.m2.pfj, df.jets.month.m2.sjs, df.jets.month.m2.stj,
-   df.jets.month.m3.pfj, df.jets.month.m3.sjs, df.jets.month.m3.stj)
 
-## U-V-Datensatz
+## Mergen des Jet-Dataframes mit dem Seeeis-Dataframe
+df.jets.month <- merge(df.jets.month, df.sea.ice, 
+                       by.x = c("year", "month"), 
+                       by.y = c("year", "month"), 
+                       all.x = TRUE)
+# Sortieren nach Datum und Längengrad
+df.jets.month <- df.jets.month[order(df.jets.month$dts, df.jets.month$lon),]
+
+
+## U-V-Datensatz ####
 # Melten
-df.uv <- melt(u[,,p.lvl,], varnames = c("lon", "lat", "t.stp"), value.name = "u")
+df.uv <- melt(u[,,p.lvl,], varnames = c("lon", "lat", "dts"), value.name = "u")
+df.uv$dts <- rep(dts, each = n.lon*n.lat)
 df.uv$v <- melt(data = v[,,p.lvl,])$value
 df.uv$uv <- sqrt( df.uv$u ** 2 + df.uv$v ** 2 )
 df.uv$z <- melt(data = z[,,p.lvl,])$value
 
 
-
-## EINFLUSS DER OROGRAPHIE AUF ZONALWIND ####
-
-## Orographie
-source("e-get-orography.r")
-df.zh <- get.orography.df()
-
-
-# 
-# ## Coriolis-Parameter
-# # f <- 2 * 0.7272 * 10 ** -4 * lat 
-# 
-# u.max <- 2 * 0.7272 * 10 ** -4 * df.jets.month$STJ.lat.m2c / 
-#   (df.jets.month$STJ.z.m2c) - get.orography.ll(df.zh = df.zh, lon = df.jets.month$lon, lat = df.jets.month$STJ.lat.m2c))
-# 
-# get.orography.ll(df.zh = df.zh, lon = df.jets.month$lon, lat = df.jets.month$STJ.lat.m2c)
-# 
-# get.orography.ll(df.zh = df.zh, lon = lon[1], lat = lat[1])
-# #df.jets.month$u.max <- 
-#  
-
-
-
-
-## SAISONALES MITTEL ÜBER ALLE JAHRE ####
+## DATAFRAMES ALS TIBBLES ####
 ## 
+tb.jets.month <- as_tibble(df.jets.month)
+tb.uv <- as_tibble(df.uv)
 
-
-
-## SAISONALES JÄHRLICHES MITTEL ####
-##
-
-
-
-## SAISONALES JÄHRLICHES MITTEL ABZGL DES ZEITLICHEN MITTELS ####
-## 
-
-
-
-## MERIDIONALES MITTEL ####
-## 
-
-
-
-## SAISONALES GLEITENDES MITTEL ÜBER FÜNF JAHRE ####
-##
-
-
-
-#### SAISONALES GLEITENDES MITTEL ABZGL DES ZEITLICHEN MITTELS ####
-## 
-
-
-
-## MERIDIONALES GLEITENDES MITTEL ####
-## 
-
-
-
-## ZEITLICHE MITTEL DER JET-POSITIONEN ####
-##
-
-
+tb.jets.month$method <- factor(tb.jets.month$method, levels = c("Max", "Chebyshev", "Dijkstra"), 
+                               labels = c("Maximum", "Chebyshev", "Dijkstra"))
+tb.jets.month$class <- factor(tb.jets.month$class, levels = c("MJ", "PFJ", "SJ", "STJ"), 
+                              labels = c("MJ", "PFJ", "SJ", "STJ"))
 
 ## LÖSCHEN UNNÖTIGER VARIABLEN ####
 ## 
 
-rm(list = setdiff(ls(), list("df.jets.cheb.all", "df.jets.month", 
-                             "df.jets.tim.mean", "df.jets.tim.mer.mean", "df.jets.season.mean", 
-                             "df.jets.season", "df.jets.season.rel", "df.jets.season.mer", 
-                             "df.jets.season.rn", "df.jets.season.rel.rn", "df.jets.season.mer.rn",
-                             "df.uv", "df.zh",
+rm(list = setdiff(ls(), list("tb.jets.month", "tb.uv", 
                              "dts", "dts.cld.wrm", "dts.month", "dts.season", "dts.year", 
                              "get.orography.df", "get.orography.ll", 
                              "i.stp", "lat", "lev", "lon", "n.lat", "n.lon", "n.seas", "n.years", "p.lvl")))
@@ -286,5 +276,6 @@ ls()
 # Speichern
 save.image("stp-b.RData")
 # load("stp-b.RData")
+
 
 ## ENDE ENDE ENDE ####
