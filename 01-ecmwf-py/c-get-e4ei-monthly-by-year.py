4649ce6a9251650156e05441e363d18fec8313c4
Sebastian Kiefer <s.kiefer@posteo.de>
2017-09-30 20:38:29 +0200

    Finalisierung der Skripte zum Download und zur Verarbeitung der ERA-Datensätze.

--- 01-ecmwf-py/c-get-e4ei-monthly-by-year.py
+++ 01-ecmwf-py/c-get-e4ei-monthly-by-year.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env py27
 
-from ecmwfapi import ECMWFDataServer
+import datetime as dt
 
 import numpy as np
-import datetime as dt
-import calendar
+from ecmwfapi import ECMWFDataServer
 
 server = ECMWFDataServer()
 
-dir = "/automount/agh/Projects/skiefer/"
+dir = "/automount/agh/Projects/skiefer/02-raw-by-year/"
 
 year_first = 1957
 year_last = 1978
 
 years_vec = np.arange(year_first, (year_last + 1))
-months_vec = np.arange(1, 13)
 
 
 for i in range(len(years_vec)):
-    date_loop_start = dt.date(years_vec[i], 1, 1)
-    date_loop_end = dt.date(years_vec[i], 12, 31)
-    date_loop = str(date_loop_start)+'/to/'+str(date_loop_end)
-    target_loop = dir+str(date_loop_start.year)+'-e4.nc'
+    date_loop = str(dt.date(years_vec[i], 1, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 2, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 3, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 4, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 5, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 6, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 7, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 8, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 9, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 10, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 11, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 12, 1).strftime('%Y%m%d'))
+    target_loop = dir+str(years_vec[i])+'-e4.nc'
     server.retrieve({
-    	"class": "e4",
-    	"dataset": "era40",
+        "class": "e4",
+        "dataset": "era40",
         "format": "netcdf",
-    	"date": date_loop,
-	"grid": "1.00/1.00",
-        'levelist': "100/150/200/250/300/400",
-    	"levtype": "pl",
-    	"param": "129.128/130.128/131.128/132.128/135.128/155.128",
-    	"step": "0",
-    	"stream": "moda",
-    	"type": "an",
-    	"target": target_loop,
+        "date": date_loop,
+        "grid": "1.00/1.00",
+        "levelist": "100/150/200/250/300/400",
+        "levtype": "pl",
+        "param": "129.128/130.128/131.128/132.128/135.128/155.128",
+        "step": "0",
+        "stream": "moda",
+        "type": "an",
+        "target": target_loop
     })
 print('E4-datasets successfully downloaded.')
 
@@ -43,26 +50,33 @@ year_first = 1979
 year_last = 2017
 
 years_vec = np.arange(year_first, (year_last + 1))
-months_vec = np.arange(1, 13)
 
 for i in range(len(years_vec)):
-    date_loop_start = dt.date(years_vec[i], 1, 1)
-    date_loop_end = dt.date(years_vec[i], 12, 31)
-    date_loop = str(date_loop_start)+'/to/'+str(date_loop_end)
-    target_loop = dir+str(date_loop_start.year)+'-ei.nc'
+    date_loop = str(dt.date(years_vec[i], 1, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 2, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 3, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 4, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 5, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 6, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 7, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 8, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 9, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 10, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 11, 1).strftime('%Y%m%d')) + '/' + \
+        str(dt.date(years_vec[i], 12, 1).strftime('%Y%m%d'))
+    target_loop = dir+str(years_vec[i])+'-ei.nc'
     server.retrieve({
-        'class': "ei",
-        'dataset': "interim",
+        "class": "ei",
+        "dataset": "interim",
         "format": "netcdf",
-    	"date": date_loop,
-	"grid": "1.00/1.00",
-        'levelist': "100/150/200/250/300/400",
-    	"levtype": "pl",
-    	"param": "129.128/130.128/131.128/132.128/135.128/155.128",
-    	"step": "0",
-    	"stream": "moda",
-    	"type": "an",
-    	"target": target_loop,
+        "date": date_loop,
+        "grid": "1.00/1.00",
+        "levelist": "100/150/200/250/300/400",
+        "levtype": "pl",
+        "param": "129.128/130.128/131.128/132.128/135.128/155.128",
+        "step": "0",
+        "stream": "moda",
+        "type": "an",
+        "target": target_loop
     })
 print('EI-datasets successfully downloaded.')
-
--- 02-cdo-bash/a-process-raw-data.sh
+++ 02-cdo-bash/a-process-raw-data.sh
@@ -1,76 +1,30 @@
 #!/bin/bash
 
-
 # change working directory
-cd /automount/agh/Projects/skiefer/01-raw-by-month/	####
-
-# merge monthly datasets to yearly datasetsi
-#cdo -b F32 mergetime 1957* ../02-raw-by-year/1957-e4.nc
-#cdo -b F32 mergetime 1957* ../02-raw-by-year/1957-e4.nc
-#cdo -b F32 mergetime 2012* ../02-raw-by-year/2012-ei.nc
-#cdo -b F32 mergetime 2013* ../02-raw-by-year/2013-ei.nc
-#cdo -b F32 mergetime 2014* ../02-raw-by-year/2014-ei.nc
-#cdo -b F32 mergetime 2016* ../02-raw-by-year/2016-ei.nc
-#cdo -b F32 mergetime 2016* ../02-raw-by-year/2016-ei.nc
+cd /automount/agh/Projects/skiefer/02-raw-by-year/
 
 # change wd to folder with yearly datasets		####
 cd ../02-raw-by-year/
 
 # merge yearly datasets to at-a-stretch-dataset
-#cdo -b F32 mergetime *.nc ../03-raw-full/a-1957-2016-e4ei-1deg.nc
+cdo -b F32 mergetime *.nc ../03-raw-full/a-1957-2016-e4ei-1deg.nc
 
 # change wd to complete dataset				####
 cd ../03-raw-full/
 
+# get information about dataset
+cdo sinfo a-1957-2016-e4ei-1deg.nc
+cdo griddes a-1957-2016-e4ei-1deg.nc
+cdo pardes a-1957-2016-e4ei-1deg.nc
+cdo showtimestamp a-1957-2016-e4ei-1deg.nc
+
 # interpolate to t63-grid
-#cdo remapbil,t63grid a-1957-2016-e4ei-1deg.nc b-1957-2016-e4ei-t63.nc
-
-# calculate daily means
-#cdo daymean b-1957-2016-e4ei-t63.nc c-1957-2016-e4ei-t63-daymean.nc
-
-# interpolate to t63 and calculate daily means in one step
-# cdo -daymean -remapbil,t63grid a-1957-2016-e4ei-1deg.nc b-1957-2016-e4ei-t63-daymean.nc
+cdo remapbil,t63grid a-1957-2016-e4ei-1deg.nc b-1957-2016-e4ei-t63-monmean.nc
 
 # select parameters u,v, select grid northern hemisphere, and invert latitude  
-cdo -invertlat -sellonlatbox,0,360,90,0 -selparam,-1,-5,-6 b-1957-2016-e4ei-t63-daymean.nc c-1957-2016-e4ei-t63-daymean-zuv-nh.nc
+cdo -invertlat -sellonlatbox,0,360,90,0 -selparam,-1,-5,-6 b-1957-2016-e4ei-t63-monmean.nc c-1957-2016-e4ei-t63-monmean-zuv-nh.nc
 
 # change wd to root-directory				####
 cd ../
-
-# calculate fields of mean and sd
-# cdo -timmean 03-raw-full/c-1957-2016-e4ei-t63-daymean-uv-nh.nc a-1957-2016-e4ei-t63-uv-nh-timmean.nc
-# cdo -timstd 03-raw-full/c-1957-2016-e4ei-t63-daymean-uv-nh.nc a-1957-2016-e4ei-t63-uv-nh-timsd.nc
-
-# monthly means
-cdo -monmean 03-raw-full/c-1957-2016-e4ei-t63-daymean-zuv-nh.nc b-1957-2016-e4ei-t63-zuv-nh-monmean.nc
-# cdo -ymonmean 03-raw-full/c-1957-2016-e4ei-t63-daymean-uv-nh.nc b-1957-2016-e4ei-t63-uv-nh-ymonmean.nc
-# cdo -monstd 03-raw-full/c-1957-2016-e4ei-t63-daymean-uv-nh.nc b-1957-2016-e4ei-t63-uv-nh-monsd.nc
-# cdo -ymonstd 03-raw-full/c-1957-2016-e4ei-t63-daymean-uv-nh.nc b-1957-2016-e4ei-t63-uv-nh-ymonsd.nc
-
-# seasonal means
-# cdo -seasmean 03-raw-full/c-1957-2016-e4ei-t63-daymean-uv-nh.nc c-1957-2016-e4ei-t63-uv-nh-seasmean.nc
-# cdo -yseasmean 03-raw-full/c-1957-2016-e4ei-t63-daymean-uv-nh.nc c-1957-2016-e4ei-t63-uv-nh-yseasmean.nc
-# cdo -seasstd 03-raw-full/c-1957-2016-e4ei-t63-daymean-uv-nh.nc c-1957-2016-e4ei-t63-uv-nh-seassd.nc
-# cdo -yseasstd 03-raw-full/c-1957-2016-e4ei-t63-daymean-uv-nh.nc c-1957-2016-e4ei-t63-uv-nh-yseassd.nc
-
-# seasonal running means
-# cdo -b F32 -mergetime -runmean,2 -selseason,DJF c-1957-2016-e4ei-t63-uv-nh-seasmean.nc -runmean,2 -selseason,MAM c-1957-2016-e4ei-t63-uv-nh-seasmean.nc -runmean,2 -selseason,JJA c-1957-2016-e4ei-t63-uv-nh-seasmean.nc -runmean,2 -selseason,SON c-1957-2016-e4ei-t63-uv-nh-seasmean.nc d-1957-2016-e4ei-t63-uv-nh-seasmean-runmean.nc
-
-
-## Orographie-Datenei-invariant.nc
-#cdo selparam,-5 e4-invariant.nc e4-orography.nc
-#cdo selparam,-6 ei-invariant.nc ei-orography.nc
-#cdo -v -W -b F32 mergetime e4-orography.nc ei-orography.nc e4ei-orography.nc
-#cdo timmean e4ei-orography.nc e4ei-geopotential.nc
-#cdo -invertlat -remapbil,t63grid e4ei-geopotential.nc e4ei-t63-geopotential.nc
-
-
-
-
-
-
-
-
-
-
+cp 03-raw-full/c-1957-2016-e4ei-t63-monmean-zuv-nh.nc ./
 
